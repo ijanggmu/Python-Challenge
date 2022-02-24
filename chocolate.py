3519a2e6d89caff81a8092edfac01a86daef8f10
Ejan <sthaejan00@gmail.com>
2022-02-25 00:36:57 +0545

    chocolate

--- chocolate.py
+++ chocolate.py
@@ -1,22 +1,12 @@
 # Replace ___ with your code
 
-# take three float input for number1, number2, number3 respectively
-n1 = float(input('no.1'))
-n2 = float(input('no.2'))
-n3 = float(input('no.3'))
-# print the average using formula_
-avg = (n1+n2+n3)/3
-
-# print calculated average
-print(avg)
-# Replace ___ with your code
-
 # take two integer inputs for chocolates and children
 choc = int(input('chocolates'))
 child = int(input('no. of childrens'))
 
 # find chocolates each children will get and and print it
-g_choc = choc/child
+g_choci = choc/child
+g_choc = int(g_choci)
 print(g_choc)
 
 # find remaining chocolates and print it
