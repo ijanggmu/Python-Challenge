47f63bb4c9484ce7729e57a9c035247fa255a5ac
Ejan <sthaejan00@gmail.com>
2022-02-25 00:24:39 +0545

    Area of Traingle

---
+++ area_of_traingle.py
@@ -0,0 +1,10 @@
+# Replace ___ with your code
+
+# take input for base and height
+b = float(input('base'))
+h = float(input('height'))
+# calculate the area
+area = 1/2*b*h
+
+# print area
+print(area)
