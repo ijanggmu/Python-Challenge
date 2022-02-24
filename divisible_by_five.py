85b1668a82a35c0fedb9736ac9e38ffdeb336848
Ejan <sthaejan00@gmail.com>
2022-02-25 01:38:06 +0545

    divisible by five

---
+++ divisible_by_five.py
@@ -0,0 +1,8 @@
+# replace ___ with your code below
+
+# Get integer input from the user
+number = int(input('number'))
+
+# print True if number is divisible by 5, otherwise print False
+# Hint: You may have to use  the % operator
+print(number % 5 == 0)
