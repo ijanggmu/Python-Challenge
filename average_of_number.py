8a1b86e46fd7010fbb9cc62b1eebd45582c383c4
Ejan <sthaejan00@gmail.com>
2022-02-25 00:28:08 +0545

    average of number

---
+++ average_of_number.py
@@ -0,0 +1,11 @@
+# Replace ___ with your code
+
+# take three float input for number1, number2, number3 respectively
+n1 = float(input('no.1'))
+n2 = float(input('no.2'))
+n3 = float(input('no.3'))
+# print the average using formula_
+avg = (n1+n2+n3)/3
+
+# print calculated average
+print(avg)
