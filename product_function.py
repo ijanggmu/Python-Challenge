f56a3faa72719f8a945a36b9719a24078c08c2b9
Ejan <sthaejan00@gmail.com>
2022-02-26 03:17:20 +0545

    added

---
+++ product_function.py
@@ -0,0 +1,16 @@
+# replace ___ with your code below
+
+# function definition
+def get_product(number1, number2):
+    result = number1*number2
+    return result
+
+
+# Get integer input from the user
+n1 = int(input('number'))
+n2 = int(input('number'))
+
+# call the function with n1 and n2 as arguments
+# store the returned value in the total variable
+total = get_product(n1, n2)
+print(total)
